b6af18dc38b3a098e8acf6a3556c41f5ad541256
niconymanda <niconymand@gmail.com>
2025-07-24 17:54:31 +0200

    test

---
+++ connect_chatbot.js
@@ -0,0 +1,33 @@
+async function getResponse() {
+    const input = document.getElementById("userQuery");
+    const responseDiv = document.getElementById("chatResponse");
+    const query = input.value.trim();
+  
+    // if (!query) {
+    //   responseDiv.innerText = "Please enter a question!";
+    //   return;
+    // }
+  
+    responseDiv.innerText = "Thinking...";
+  
+    try {
+      const res = await fetch("https://replit.com/@niconymand/website-chatbot", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query: query })
+      });
+  
+      if (!res.ok) {
+        throw new Error("Server error: " + res.status);
+      }
+  
+      const data = await res.json();
+      responseDiv.innerText = data.answer || "No answer returned.";
+    } catch (err) {
+      console.error(err);
+      responseDiv.innerText = "Sorry, something went wrong.";
+    }
+  }
+  
